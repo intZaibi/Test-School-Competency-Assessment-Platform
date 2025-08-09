9f0d439232aa1125aa42809bb22b3bd76d2f3c52
Zaibi <shahzaibalisomroo@gmail.com>
2025-08-09 14:18:40 +0500

    Delete backend/dist directory

--- backend/dist/routes/assessmentRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const assessmentController_js_1 = require("../controllers/assessmentController.js");
-const router = (0, express_1.Router)();
-router.post("/", assessmentController_js_1.createAssessment);
-router.get("/", assessmentController_js_1.getAllAssessments);
-router.get("/:step/:level", assessmentController_js_1.getQuestionsByLevel);
-exports.default = router;
