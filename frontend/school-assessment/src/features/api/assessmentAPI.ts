ce5e4f77da4dd351482a695c2520d9f8313bf237
Zaibi <shahzaibalisomroo@gmail.com>
2025-08-11 15:51:26 +0500

    Update assessmentAPI.ts

--- frontend/school-assessment/src/features/api/assessmentAPI.ts
+++ frontend/school-assessment/src/features/api/assessmentAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/assessments",
+  baseURL: "https://test-school-competency-assessment.onrender.com/api/assessments",
   withCredentials: true,
 });
 
